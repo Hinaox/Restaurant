a4c32258817c8c01b417305784c9a2d2d44340a8
Faniry <faniryrandriamparany@gmail.com>
2022-03-22 11:45:56 +0300

    baseWithPhoto

--- base.sql
+++ base.sql
@@ -1,11 +1,20 @@
 create database restaurant;
+create table produit(
+	id varchar(20) primary key,
+	nom varchar(100)
+);
 create table categorie(
 	id varchar(20) primary key,
 	nom varchar(100)
 );
+create table photo(
+	id varchar(20) primary key,
+	nom varchar(255)
+);
 create table plat(
 	id varchar(20) primary key,
 	nom varchar(255),
+	idPhoto varchar(20),
 	prix double precision
 );
 create table categoriePlat(
@@ -13,12 +22,25 @@ create table categoriePlat(
 	idPlat varchar(20),
 	idCategorie varchar(20)
 );
-create sequence platseq start with 1 increment by 1;
+create table platProduit(
+	idPlat varchar(20),
+	idProduit varchar(20),
+	qte double precision
+);
+create sequence produitseq  start with 1 increment by 1;
 create sequence categorieseq  start with 1 increment by 1;
+create sequence photoseq  start with 1 increment by 1;
+create sequence platseq start with 1 increment by 1;
 create sequence categoriePlatseq  start with 1 increment by 1;
+create sequence platProduitseq  start with 1 increment by 1;
 
-alter table ADD CONSTRAINT fk_categorieplat_plat foreign key (idPlat) references plat(id);
-alter table ADD CONSTRAINT fk_categorieplat_categorie foreign key (idCategorie) references categorie(id);
+alter table categorieplat ADD CONSTRAINT fk_categorieplat_plat foreign key (idPlat) references plat(id);
+alter table categorieplat ADD CONSTRAINT fk_categorieplat_categorie foreign key (idCategorie) references categorie(id);
+
+alter table platProduit ADD CONSTRAINT fk_categorieplat_categorie foreign key (idPlat) references plat(id);
+alter table platProduit ADD CONSTRAINT fk_categorieplat_categorie foreign key (idProduit) references produit(id);
+
+alter table plat ADD CONSTRAINT fk_plat_photo foreign key (idPhoto) references photo(id);
 
 create view categorieDetails as 
 	select categorie.nom,categoriePlat.* from categoriePlat inner join categorie on categoriePlat.idCategorie = categorie.id
@@ -39,14 +61,14 @@ insert into categorie values(CONCAT('Categorie',nextVal('categorieseq')),'Entree
 insert into categorie values(CONCAT('Categorie',nextVal('categorieseq')),'Plat');
 insert into categorie values(CONCAT('Categorie',nextVal('categorieseq')),'Dessert');
 
-insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat17','Categorie3');
-insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat18','Categorie3');
+insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat7','Categorie3');
+insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat8','Categorie3');
 
-insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat15','Categorie1');
-insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat16','Categorie1');
+insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat5','Categorie1');
+insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat6','Categorie1');
 
-insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat12','Categorie1');
-insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat13','Categorie1');
+insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat1','Categorie1');
+insert into categoriePlat values(CONCAT('categPlat',nextVal('categoriePlatseq')),'Plat2','Categorie1');
 
 
 	
