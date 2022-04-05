e777b5997062eaf454268b9dc34109ff540fbbbd
Amboara01 <amboara.ralaiarimanana@outlook.com>
2022-04-05 09:30:56 +0300

    DAO

---
+++ Restaurant/src/java/db/ConditionBuilder.java
@@ -0,0 +1,15 @@
+package utility;
+
+public class ConditionBuilder {
+    private String dateFormat;
+
+    public ConditionBuilder setDateFormat(String dateFormat) {
+        this.dateFormat = dateFormat;
+        return this;
+    }
+
+    public Condition build(){
+        Condition condition = new Condition(dateFormat);
+        return condition;
+    }
+}
